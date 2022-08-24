b4198434bdee5a81aa878a9ec629b4b19b5d6bcd
manzeelanepali <nepalimanzeela@gmail.com>
2022-08-24 15:34:57 +0545

    accessing elements through use of map

---
+++ phonebook/src/App.js
@@ -0,0 +1,41 @@
+
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number:"567890-09876" },{
+      name: 'Arto Hllas', number:"67890-09876" 
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+  console.log(persons)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div> {
+        // Map makes new empty array and "x inside of paranthesis ma chai sabai array ko elements one by one ahucha 
+        // .<li> le bulllet ko form ma ahucha "
+       
+        persons.map((x)=>(
+          <li>{x.name} {x.number}</li>
+        ))}
+        
+      </div>
+     
+  {/* {[<li>manjila</li>,<li>niru</li>]} */}
+    
+    </div>
+  )
+}
+
+export default App
